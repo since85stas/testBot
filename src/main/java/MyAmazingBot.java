8abcb116ffd33f8dc98355e634c5433948fd95a3
Note <seeyou85@gmail.com>
2022-03-19 16:19:40 +0300

    простейший бот

--- src/main/java/MyAmazingBot.java
+++ src/main/java/MyAmazingBot.java
@@ -1,25 +1,36 @@
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class MyAmazingBot extends TelegramLongPollingBot {
 
-    public MyAmazingBot() {
-        super();
-    }
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText("Мяууууу");
 
-    public MyAmazingBot(DefaultBotOptions options) {
-        super(options);
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
-    public void clearWebhook() throws TelegramApiRequestException {
-        super.clearWebhook();
+    public String getBotUsername() {
+        return "Beliash1_bot";
     }
 
     @Override
-    public void onClosing() {
-        super.onClosing();
+    public String getBotToken() {
+        return "5204596368:AAH99ayR0vZn-gxUMvyzcctaNbw41k_C-zo";
     }
+
 }
